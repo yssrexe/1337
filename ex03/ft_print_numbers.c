95204b5fbf143c6adb233f6599f4b444dbb8a085
yssrexe <elyasiniyassir40@gmail.com>
2023-06-18 11:36:45 +0100

    test

---
+++ ex03/ft_print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void ft_print_numbers(void)
+	{
+		int c = 0;
+		while()
+		{
+			write(1, &c , 1);
+			c++;
+		}
+	}
---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void ft_is_negative(int n)
+{
+	if(n = true)
+	{
+		write(1 ,&"P" ,1);
+	} else
+	{
+		write(1 ,&"N" ,1);
+	}
+
+}
