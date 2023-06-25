6e46c599aa065472eb8fc1ec77ba069c04b51f2e
yssrexe <elyasiniyassir40@gmail.com>
2023-06-25 12:27:40 +0100

    test

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+
+void ft_rev_int_tab(int *tab, int size)
+{
+	int i ;
+	int swap ;
+
+	i = 0;
+	while(i < (size / 2))
+	{
+		swap = tab[i];
+		tab[i] = tab [size - 1 - i];
+		tab [size - 1 - i] = swap;
+		i++;
+	}
+}
