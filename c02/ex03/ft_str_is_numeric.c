99c0f198501dc4b92c5b729ae95ddd33abe89ba2
yssrexe <elyasiniyassir40@gmail.com>
2023-06-26 12:32:08 +0100

    test

---
+++ c02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,22 @@
+#include<unistd.h>
+
+int ft_str_is_numeric(char *str)
+{
+	int i;
+	i = 0;
+
+	if(str[i] == '\0')
+	{
+		return (1);
+	}
+
+	while(str[i] != '\0')
+	{
+		if(!(str[i] >= 48 && str[i] <= 57))
+		{
+			return (0);
+		}
+		i++
+	}
+	return (1);
+}
