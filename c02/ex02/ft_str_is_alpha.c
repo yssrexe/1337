7b9b568c718efd8e2a5f84d610b1b85737d0d5dc
yssrexe <elyasiniyassir40@gmail.com>
2023-06-26 11:29:57 +0100

    test

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+
+int ft_str_is_alpha(char *str)
+{
+	int i;
+	
+	i = 0;
+
+	if(str[i] == '\0')
+	{
+		return 0;
+	}
+	while(str[i] != '\0')
+	{
+
+		if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+        	{
+               		i++;
+		} else 
+		{
+			return(0);
+		}
+		return(1);
+	}
+}
