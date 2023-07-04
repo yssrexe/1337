ca0df0a6f3706ed0075adb59212eda03c892671a
yssrexe <elyasiniyassir40@gmail.com>
2023-07-04 12:49:52 +0100

    test

---
+++ c05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,16 @@
+int ft_recursive_factorial(int nb)
+{
+	int res;
+
+	res = 1;
+
+	if(nb < 0)
+		return(0);
+
+	if(nb < 2)
+		return(1);
+
+	res = nb * ft_recursive_factorial(nb - 1);
+	return(res);
+	
+}
