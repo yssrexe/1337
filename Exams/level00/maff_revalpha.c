00ca38f75fcd19e16007a363b35b984478739341
yssrexe <elyasiniyassir40@gmail.com>
2023-12-13 18:17:09 +0100

    1337

---
+++ Exams/level00/maff_revalpha.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+int main()
+{
+    int num = 0;
+    char alfa1 = 122;
+    char alfa2 = 89;
+
+    while (num < 13)
+    {
+        write(1, &alfa1, 1);
+        write(1, &alfa2, 1);
+
+        alfa1 = alfa1 - 2;
+        alfa2 = alfa2 - 2;
+        
+        num++;
+    }
+    write(1,  "\n", 1);
+}
