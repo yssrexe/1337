44afb9e28eb9a5a2e6ed3728fe2078816ffc7cc9
yssrexe <elyasiniyassir40@gmail.com>
2023-12-17 17:42:03 +0100

    1337

---
+++ Exams/levels/level01/ft_strcpy.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+    int i = 0;
+    while (s1[i])
+    {
+        s2[i] == s1[i];
+        i++;
+    }
+    s2[i] = '\0';
+    return (s2);
+}
